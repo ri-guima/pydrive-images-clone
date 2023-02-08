34eded189d8ad39e9cbf9036cd865e5952743392
ri-guima <richard.alexsander.guima@gmail.com>
2023-02-08 12:15:54 -0300

    feat: first version works

--- image_api/utils.py
+++ image_api/utils.py
@@ -1,40 +1,39 @@
 from flask import url_for
 from pydrive.auth import GoogleAuth
+from dotenv import load_dotenv
+import os
+from pathlib import Path
 from pydrive.drive import GoogleDrive
 
 
 def is_image(file):
     allowed_extensions = ['jpg', 'jpeg', 'png']
-    return file.get('extensions') in allowed_extensions
+    return file.get('fileExtension') in allowed_extensions
 
 
 def to_dict(file):
     return {'id': file['id'], 'url': url_for_image(file)}
 
 
-def save_image(file):
-    pass
-
-
 def url_for_image(image):
-    filename = f'uploads/{image["id"]}.{image["extension"]}'
-    return url_for('.static', filename=filename)
+    return url_for('.static', filename=f'uploads/{get_filename(image)}')
 
 
-def get_drive_files():
-    drive = get_drive()
+def save_image(file):
+    load_dotenv('.env')
+    file.GetContentFile(Path(os.getenv('UPLOAD_FOLDER')) / get_filename(file))
+
+
+def get_filename(file):
+    return f'{file["id"]}.{file["fileExtension"]}'
+
+
+def get_drive_files(drive):
     return drive.ListFile({'q': '"root" in parents and trashed=false'}).GetList()
 
 
-def get_drive():
+def create_drive():
     gauth = GoogleAuth()
-    gauth.LoadCredentialsFile('credentials.txt')
-    if gauth.credentials is None:
-        gauth.LocalWebserverAuth()
-    elif gauth.access_token_expired:
-        gauth.Refresh()
-    else:
-        gauth.Authorize()
-    gauth.SaveCredentialsFile('credentials.txt')
     gauth.LocalWebserverAuth()
+    gauth.LoadCredentialsFile('credentials.json')
     return GoogleDrive(gauth)
