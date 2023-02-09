91c4161586155674fe8f6fa335007d2e9e531a73
ri-guima <richard.alexsander.guima@gmail.com>
2023-02-09 12:49:03 -0300

    fix: remove help of testing argument

--- images_clone/main.py
+++ images_clone/main.py
@@ -8,8 +8,7 @@ from domain import save_image
 if __name__ == '__main__':
     parser = ArgumentParser(description='Google Drive images clone')
     parser.add_argument('-d', '--dir', type=str, help='Clone directory')
-    parser.add_argument('-t', '--testing', help='Run with fake drive',
-                        action='store_true')
+    parser.add_argument('-t', '--testing', action='store_true')
     args = parser.parse_args()
     drive = Drive()
     if args.testing:
