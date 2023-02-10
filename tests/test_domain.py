122e5ea1840ff26148caae4e51a1972ba6acdc08
ri-guima <richard.alexsander.guima@gmail.com>
2023-02-10 18:48:44 -0300

    tests: change tests

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -13,7 +13,7 @@ def test_is_image():
 
 
 def test_save_image():
-    image = Image(id='12345', extension='jpg', content='Hello World')
+    image = Image(id='12345', extension='jpg', content=bytes('Hello World', 'utf-8'))
     dir = 'tests/uploads'
     save_image(Path(dir), image)
     assert '12345.jpg' in os.listdir(dir)
